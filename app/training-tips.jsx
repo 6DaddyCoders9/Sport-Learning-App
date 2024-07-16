d20c6a145893a6bccc91de0b98824999b2d4a5fb
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-07-16 23:51:25 +0530

    Added Landing Page and Basic Layout

---
+++ app/training-tips.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TrainingTips = () => {
+  return (
+    <View>
+      <Text>TrainingTips</Text>
+    </View>
+  )
+}
+
+export default TrainingTips
